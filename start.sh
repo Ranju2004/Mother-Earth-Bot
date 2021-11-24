5d6ebf06329113e3d3fc4d2be770b317fef82ad7
LɪᴏɴKᴇᴛᴛʏUᴅ <88967484+LionKettyUD@users.noreply.github.com>
2021-11-24 21:16:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Mother-Earth-Bot
 fi
-cd /EvaMaria
+cd /Mother-Earth-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
