fc61d38237674a3656a8ed53b54740ff4171de7c
FHKL-Bots432 <96911602+FHKL-Bots432@users.noreply.github.com>
2022-01-22 17:10:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LionKettyUD/Mother-Earth-Bot.git /Mother-Earth-Bot
+  git clone https://github.com/FHKL-Bots432/Mother-Earth-Bot.git /Mother-Earth-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Mother-Earth-Bot
