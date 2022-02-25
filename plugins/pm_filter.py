5e4659d62b32926c70b14692a3f011db5ef8628b
FHKL-Bots432 <96911602+FHKL-Bots432@users.noreply.github.com>
2022-02-25 20:20:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -442,8 +442,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('sᴏᴜʀᴄᴇ', callback_data='source')
         ], [
             InlineKeyboardButton('𝖧𝗈𝗆𝖾', callback_data='start'),
-            InlineKeyboardButton('𝐒𝐭𝐚𝐭𝐮𝐬 🌡', callback_data='stats')
-            InlineKeyboardButton('𝙘𝙡𝙤𝙨𝙚 😑', callback_data='close_data')
+            InlineKeyboardButton('𝐒𝐭𝐚𝐭𝐮𝐬', callback_data='stats')
+            InlineKeyboardButton('𝙘𝙡𝙤𝙨𝙚', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
