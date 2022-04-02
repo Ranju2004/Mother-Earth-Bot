db093a6db5f254c37e7764738acc8f098d6d532d
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-04-02 11:28:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton(' 𝘼𝙙𝙙 𝙈𝙚 𝙏𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥s 👥', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('𝗙𝗶𝗹𝗺𝘆 𝗛𝗮𝗻𝗴𝗮𝗺𝗮', url='https://t.me/Filmy_hangama')
+            InlineKeyboardButton('Channel', url='https://t.me/FHM_Movies')
         ], [
             InlineKeyboardButton('𝙷𝚎𝚕𝚙', callback_data='help'),
             InlineKeyboardButton('𝙰𝚋𝚘𝚞𝚝', callback_data='about')
@@ -438,8 +438,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('𝗙𝗶𝗹𝗺𝘆 𝗛𝗮𝗻𝗴𝗮𝗺𝗮', url='https://t.me/Filmy_hangama'),
-            InlineKeyboardButton('sᴏᴜʀᴄᴇ', callback_data='source')
+            InlineKeyboardButton('Channel', url='https://t.me/FHM_Movies'),
+            InlineKeyboardButton('Group', url='https://t.me/Movie_requests_kannada')
         ], [
             InlineKeyboardButton('𝖧𝗈𝗆𝖾', callback_data='start'),
             InlineKeyboardButton('Close', callback_data='close_data') 
